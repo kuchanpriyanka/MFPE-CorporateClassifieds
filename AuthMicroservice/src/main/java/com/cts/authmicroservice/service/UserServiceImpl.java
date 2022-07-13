1f2a7d4ca5d4e3a59da9e65e436d193af2637191
kuchanpriyanka <kuchanpriyanka@gmail.com>
2022-07-13 17:04:17 +0530

    initial commit

---
+++ AuthMicroservice/src/main/java/com/cts/authmicroservice/service/UserServiceImpl.java
@@ -0,0 +1,93 @@
+package com.cts.authmicroservice.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import com.cts.authmicroservice.exception.UnauthorizedException;
+import com.cts.authmicroservice.jwt.JwtUtil;
+import com.cts.authmicroservice.model.AuthResponse;
+import com.cts.authmicroservice.model.UserModel;
+import com.cts.authmicroservice.model.UserToken;
+import com.cts.authmicroservice.repository.UserRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+
+public class UserServiceImpl implements UserService {
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	JwtUtil jwtUtil;
+
+	@Override
+	public UserDetails loadUserByUsername(String userName) {
+		// all actions to check the correctness of the userID
+		log.info("Inside loadbyusername");
+		UserModel user = userRepository.findByEmpUsername(userName);
+		return new User(user.getEmpUsername(), user.getEmpPassword(), new ArrayList<>());
+	}
+
+	// authenticates the user
+	public UserToken login(UserModel userModel) {
+
+		final UserDetails userdetails = loadUserByUsername(userModel.getEmpUsername());
+
+		UserToken userToken = new UserToken();
+
+		// if the password matches
+		if (userdetails.getPassword().equals(userModel.getEmpPassword())) {
+			log.info("authentication successfull.. Generating token..");
+
+			// set the values for the token
+			userToken.setUsername(userModel.getEmpUsername());
+			userToken.setEmpid(userRepository.findByEmpUsername(userModel.getEmpUsername()).getEmpid());
+			userToken.setAuthToken(jwtUtil.generateToken(userdetails));
+			log.info("authentication successfull.. Generating token.." + userToken.toString());
+
+			return userToken;
+		} else {
+			log.error("authentication failed");
+			throw new UnauthorizedException("Invalid username or password");
+		}
+	}
+
+	// validates the JWT token
+	public AuthResponse getValidity(String token) {
+		// retrieving the token ( removing the Bearer from the header)
+		AuthResponse authResponse = new AuthResponse();
+		if(null != token && token.length() >= 7) {
+		String token1 = token.substring(7);
+		// if valid
+		if (jwtUtil.validateToken(token1)) {
+			log.info("Token is valid");
+
+			// extract the user name
+			String username = jwtUtil.extractUsername(token1);
+
+			// set the values for the response
+			authResponse.setUsername(username);
+			authResponse.setValid(true);
+			authResponse.setEmpid(userRepository.findByEmpUsername(username).getEmpid());
+		} else {
+			authResponse.setValid(false);
+			log.error("Token is invalid or expired...");
+		}
+		} else {
+			authResponse.setValid(false);
+			log.error("Token is invalid or expired...");
+		}
+		return authResponse;
+	}
+
+//	public int getEmpId(String username) {
+//		log.info("Inside getemployeeid");
+//		return userRepository.findByEmpUsername(username).getEmpid();
+//	}
+}
---
+++ OfferMicroservice/src/main/resources/data.sql
@@ -0,0 +1,60 @@
+drop table if exists liked_by;
+drop table if exists offer;
+drop table if exists employee;
+
+create table Employee(
+	id int primary key auto_increment,
+    name varchar(50) not null,
+    department varchar(50),
+    gender varchar(6) not null,
+    age int not null,
+    contact_number bigint,
+    email varchar(100) unique,
+    points_gained int default 0,
+    check (gender in ('male','female','other')),
+    check ( age > 0 )
+    );
+    
+create table Offer(
+	id int primary key auto_increment,
+    name varchar(50) not null,
+    description varchar(100),
+    category varchar(50) not null,
+    open_date timestamp default now(),
+    closed_date timestamp,
+    engaged_date timestamp,
+    engaged_emp_id int,
+    emp_id int not null,
+    likes int default 0,
+    foreign key(engaged_emp_id) references Employee(id) on delete cascade,
+    foreign key(emp_id) references Employee(id) on delete cascade
+    );
+
+create table liked_by(
+	emp_id int,
+    offer_id int,
+    liked_on timestamp default now(),
+    primary key(emp_id,offer_id),
+    foreign key(emp_id) references Employee(id) on delete cascade,
+    foreign key(offer_id) references Offer(id) on delete cascade
+    );
+    
+    
+insert into Employee(id,name,gender,age) values(1,'Kaustubh','male',21);
+insert into Employee(id,name,gender,age) values(2,'Priyanka','female',22);
+insert into Employee(id,name,gender,age) values(3,'Isha','female',24);
+insert into Employee(id,name,gender,age) values(4,'Sid','male',25);
+
+insert into Offer(id,name,category,emp_id) values(1,'OFFER 1' ,'Electronics',3);
+insert into Offer(id,name,category,emp_id) values(2,'OFFER 2' ,'Hardware Devices',3);
+insert into Offer(id,name,category,emp_id) values(3,'OFFER 3','Plants',2);
+insert into Offer(id,name,category,emp_id) values(4,'OFFER 4','Furniture',1);
+insert into Offer(id,name,category,emp_id) values(5,'OFFER 5','Accessories',2);
+insert into Offer(id,name,category,emp_id) values(6,'OFFER 6','Electronics',4);
+insert into Offer(id,name,category,emp_id) values(7,'OFFER 7' ,'Electronics',1);
+insert into Offer(id,name,category,emp_id) values(8,'OFFER 8' ,'Plants',4);
+
+insert into liked_by(emp_id,offer_id) values(1,1);
+insert into liked_by(emp_id,offer_id) values(1,2);
+insert into liked_by(emp_id,offer_id) values(3,6);
+insert into liked_by(emp_id,offer_id) values(2,4);
